8da8b5b876740b95388a306bac7bb51e6748048d
conankzhang <conankzhang@gmail.com>
2018-08-25 20:54:22 -0600

    RobotJoint: Set up joint for forward kinematics

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour {
+
+    public RobotJoint[] Joints;
+
+    public Vector3 ForwardKinematics(float[] angles)
+    {
+        Vector3 prevPoint = Joints[0].transform.position;
+        Quaternion rotation = Quaternion.identity;
+
+        for (int i = 1; i < Joints.Length; i++)
+        {
+            rotation *= Quaternion.AngleAxis(angles[i - 1], Joints[i - 1].Axis);
+            Vector3 nextPoint = prevPoint + rotation * Joints[i].StartOffset;
+
+            prevPoint = nextPoint;
+        }
+
+        return prevPoint;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/RobotJoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotJoint : MonoBehaviour {
+    public Vector3 Axis;
+    public Vector3 StartOffset;
+
+    void Awake()
+    {
+        StartOffset = transform.localPosition;
+    }
+}
